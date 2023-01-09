461248263f88899dfb99fb8e85ccf0f056a642d1
Yaguang Jia <jiayaguang@foxmail.com>
2023-01-09 15:47:49 +0800

    release 3.6.7-kyarthas-r3

--- bin/as.sh
+++ bin/as.sh
@@ -11,7 +11,7 @@
 #    date : 2022-12-12
 
 # current arthas script version
-ARTHAS_SCRIPT_VERSION=3.6.7-kyarthas-r2
+ARTHAS_SCRIPT_VERSION=3.6.7-kyarthas-r3
 
 # SYNOPSIS
 #   rreadlink <fileOrDirPath>
@@ -455,7 +455,7 @@ EXAMPLES:
   ./as.sh --stat-url 'http://192.168.10.11:8080/api/stat'
   ./as.sh -c 'sysprop; thread' <pid>
   ./as.sh -f batch.as <pid>
-  ./as.sh --use-version 3.6.7-kyarthas-r2
+  ./as.sh --use-version 3.6.7-kyarthas-r3
   ./as.sh --session-timeout 3600
   ./as.sh --attach-only
   ./as.sh --disabled-commands stop,dump
